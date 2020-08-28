b007d077223f2e5d70c3743be2e5c65558f6b262
djviodes <djviodes26@gmail.com>
2020-08-27 19:56:15 -0700

    added the playRound function to script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-//begin all variables at 0
 //create a function that randomly selects "rock, paper, or scissors"
 function computerPlay() {
     const computerChoice = Math.floor(Math.random() * 3); //this function generates a random number 0, 1, or 2
@@ -13,6 +12,39 @@ function computerPlay() {
             return "There was an error with the number generator."
     }
 }
-//create an input that allows user to select "rock, paper, or scissors"
 //create a function that plays one round of the game while selecting a winner
+function playRound (playerChoice, computerChoice) {
+    playerChoice = playerChoice.toLowerCase(); //this ensures the user's choice is not case sensitive as it will automatically input as all lower case
+    computerChoice = computerChoice.toLowerCase(); //this ensures the computer's choice is not case sensitive as it will automatically input as all lower case
+    if (playerChoice == computerChoice) { //this is the choice if the user and computer choose the same option
+        return `Draw! ${playerChoice} equals ${computerChoice}`
+    }
+    else if (playerChoice == "rock") { //this is the choice if the user chooses rock
+        if (computerChoice == "scissors") {
+            return "You won! Rock beats Scissors.";
+        }
+        else if (computerChoice == "paper") {
+            return "You lost... Paper beats Rock.";
+        }
+    }
+    else if (playerChoice == "paper") { //this is the choice if the user chooses paper
+        if (computerChoice == "rock") {
+            return "You won! Paper beats Rock.";
+        }
+        else if (computerChoice == "scissors") {
+            return "You lost... Scissors beats Paper.";
+        }
+    }
+    else if (playerChoice == "scissors") { //this is the choice if the use chooses scissors
+        if (computerChoice == "paper") {
+            return "You won! Scissors beats Paper.";
+        }
+        else if (computerChoice == "rock") {
+            return "You lost... Rock beats Scissors.";
+        }
+    }
+    else {
+        return "This should not have happened."
+    }
+}
 //create a function that utilizes the one round function to play five rounds while keeping score and declares user either a winner or loser
